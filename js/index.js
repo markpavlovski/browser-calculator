fd3bc3aef45d55aabf54066abe6f5f5777a04155
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-01 13:46:01 -0800

    added length conditional

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const tilt = 5
+const displayLength = 14
 const calculator = document.querySelector("#calculator")
 const screen = document.querySelector("#screen")
 const buttons = document.querySelector(".buttons")
@@ -41,15 +42,16 @@ buttons.addEventListener("click",(event)=>{
 
     if(operators.includes(value)){
 
-      console.log("do the operation")
+      // HANDLE OPERATORS
 
       if (value === "C"){
-
+      // Deal with clear button
         displayText = ""
         operation = null
+        justCalculated = false
 
       } else if (value === "=") {
-
+      // Deal with equals button
         if (operation && displayText){
           rightNumber = Number(displayText.split(operation)[1])
           console.log(Number(rightNumber))
@@ -59,26 +61,34 @@ buttons.addEventListener("click",(event)=>{
         }
         operation = null
       } else {
-
+      // Deal with operators
         if (!operation && displayText){
           leftNumber = Number(displayText)
           console.log(Number(leftNumber))
           displayText += value
           operation = value
+          justCalculated = false
         }
 
       }
-
     } else {
+
+      // HANDLE NUMERIC INPUTS
+
       if (justCalculated){
         displayText = value
       } else {
         console.log("add another number")
-        displayText += value
+        if (displayText.length < displayLength){
+          displayText += value
+        }
       }
-
+      justCalculated = false
     }
-  // justCalculated = false
+
   }
+
+  // RENDER
   screen.innerText = displayText
+
 })
